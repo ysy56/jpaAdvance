73f7964edf66118d30f08e576e379550b109865e
moony <78634780+ysy56@users.noreply.github.com>
2024-05-23 14:50:03 +0900

    [2-10] 중간 테이블 Order 테스트

--- src/test/java/com/sparta/jpaadvance/relation/OrderTest.java
+++ src/test/java/com/sparta/jpaadvance/relation/OrderTest.java
@@ -1,8 +1,10 @@
 package com.sparta.jpaadvance.relation;
 
 import com.sparta.jpaadvance.entity.Food;
+import com.sparta.jpaadvance.entity.Order;
 import com.sparta.jpaadvance.entity.User;
 import com.sparta.jpaadvance.repository.FoodRepository;
+import com.sparta.jpaadvance.repository.OrderRepository;
 import com.sparta.jpaadvance.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,50 +13,52 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Transactional
 @SpringBootTest
-public class OneToManyTest {
+public class OrderTest {
 
     @Autowired
     UserRepository userRepository;
     @Autowired
     FoodRepository foodRepository;
+    @Autowired
+    OrderRepository orderRepository;
 
     @Test
     @Rollback(value = false)
-    @DisplayName("1대N 단방향 테스트")
+    @DisplayName("중간 테이블 Order Entity 테스트")
     void test1() {
+
         User user = new User();
         user.setName("Robbie");
-
-        User user2 = new User();
-        user2.setName("Robbert");
 
         Food food = new Food();
         food.setName("후라이드 치킨");
         food.setPrice(15000);
-//        food.getUserList().add(user); // 외래 키(연관 관계) 설정
-//        food.getUserList().add(user2); // 외래 키(연관 관계) 설정
+
+        // 주문 저장
+        Order order = new Order();
+        order.setUser(user); // 외래 키(연관 관계) 설정
+        order.setFood(food); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
-        userRepository.save(user2);
         foodRepository.save(food);
-
-        // 추가적인 UPDATE 쿼리 발생을 확인할 수 있습니다.
+        orderRepository.save(order);
     }
 
     @Test
-    @DisplayName("1대N 조회 테스트")
+    @DisplayName("중간 테이블 Order Entity 조회")
     void test2() {
-        Food food = foodRepository.findById(1L).orElseThrow(NullPointerException::new);
-        System.out.println("food.getName() = " + food.getName());
+        // 1번 주문 조회
+        Order order = orderRepository.findById(1L).orElseThrow(NullPointerException::new);
 
-        // 해당 음식을 주문한 고객 정보 조회
-//        List<User> userList = food.getUserList();
-//        for (User user : userList) {
-//            System.out.println("user.getName() = " + user.getName());
-//        }
+        // order 객체를 사용하여 고객 정보 조회
+        User user = order.getUser();
+        System.out.println("user.getName() = " + user.getName());
+
+        // order 객체를 사용하여 음식 정보 조회
+        Food food = order.getFood();
+        System.out.println("food.getName() = " + food.getName());
+        System.out.println("food.getPrice() = " + food.getPrice());
     }
 }
