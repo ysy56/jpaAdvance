a9586e7b0595c99c1b7a5fded52a0b993d6fd025
moony <78634780+ysy56@users.noreply.github.com>
2024-05-23 12:25:29 +0900

    @OneToOne 양방향 및 조회 테스트

--- src/test/java/com/sparta/jpaadvance/relation/OneToOneTest.java
+++ src/test/java/com/sparta/jpaadvance/relation/OneToOneTest.java
@@ -38,4 +38,81 @@ public class OneToOneTest {
         foodRepository.save(food);
     }
 
+    @Test
+    @Rollback(value = false)
+    @DisplayName("1대1 양방향 테스트 : 외래 키 저장 실패")
+    void test2() {
+        Food food = new Food();
+        food.setName("고구마 피자");
+        food.setPrice(30000);
+
+        // 외래 키의 주인이 아닌 User 에서 Food 를 저장해보겠습니다.
+        User user = new User();
+        user.setName("Robbie");
+        user.setFood(food);
+
+        userRepository.save(user);
+        foodRepository.save(food);
+
+        // 확인해 보시면 user_id 값이 들어가 있지 않은 것을 확인하실 수 있습니다.
+    }
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("1대1 양방향 테스트 : 외래 키 저장 실패 -> 성공")
+    void test3() {
+        Food food = new Food();
+        food.setName("고구마 피자");
+        food.setPrice(30000);
+
+        // 외래 키의 주인이 아닌 User 에서 Food 를 저장하기 위해 addFood() 메서드 추가
+        // 외래 키(연관 관계) 설정 food.setUser(this); 추가
+        User user = new User();
+        user.setName("Robbie");
+        user.addFood(food);
+
+        userRepository.save(user);
+        foodRepository.save(food);
+    }
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("1대1 양방향 테스트")
+    void test4() {
+        User user = new User();
+        user.setName("Robbert");
+
+        Food food = new Food();
+        food.setName("고구마 피자");
+        food.setPrice(30000);
+        food.setUser(user); // 외래 키(연관 관계) 설정
+
+        userRepository.save(user);
+        foodRepository.save(food);
+    }
+
+    @Test
+    @DisplayName("1대1 조회 : Food 기준 user 정보 조회")
+    void test5() {
+        Food food = foodRepository.findById(1L).orElseThrow(NullPointerException::new);
+        // 음식 정보 조회
+        System.out.println("food.getName() = " + food.getName());
+
+        // 음식을 주문한 고객 정보 조회
+        System.out.println("food.getUser().getName() = " + food.getUser().getName());
+    }
+
+    @Test
+    @DisplayName("1대1 조회 : User 기준 food 정보 조회")
+    void test6() {
+        User user = userRepository.findById(1L).orElseThrow(NullPointerException::new);
+        // 고객 정보 조회
+        System.out.println("user.getName() = " + user.getName());
+
+        // 해당 고객이 주문한 음식 정보 조회
+        Food food = user.getFood();
+        System.out.println("food.getName() = " + food.getName());
+        System.out.println("food.getPrice() = " + food.getPrice());
+    }
+
 }
