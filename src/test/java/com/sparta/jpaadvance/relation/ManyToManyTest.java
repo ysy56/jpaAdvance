fbc44d3d26bc78813b97d66f17d3732f9aaafc65
moony <78634780+ysy56@users.noreply.github.com>
2024-05-23 14:32:11 +0900

    [2-10] @ManyToMany 단방향 테스트

---
+++ src/test/java/com/sparta/jpaadvance/relation/ManyToManyTest.java
@@ -0,0 +1,46 @@
+package com.sparta.jpaadvance.relation;
+
+import com.sparta.jpaadvance.entity.Food;
+import com.sparta.jpaadvance.entity.User;
+import com.sparta.jpaadvance.repository.FoodRepository;
+import com.sparta.jpaadvance.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+public class ManyToManyTest {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    FoodRepository foodRepository;
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("N대M 단방향 테스트")
+    void test1() {
+
+        User user = new User();
+        user.setName("Robbie");
+
+        User user2 = new User();
+        user2.setName("Robbert");
+
+        Food food = new Food();
+        food.setName("후라이드 치킨");
+        food.setPrice(15000);
+        food.getUserList().add(user);
+        food.getUserList().add(user2);
+
+        userRepository.save(user);
+        userRepository.save(user2);
+        foodRepository.save(food);
+
+        // 자동으로 중간 테이블 orders 가 create 되고 insert 됨을 확인할 수 있습니다.
+    }
+}
