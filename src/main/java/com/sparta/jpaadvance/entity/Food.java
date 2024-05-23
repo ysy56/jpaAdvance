b2d74a70a1577b39764a15df45e13b424d7b6b1f
moony <78634780+ysy56@users.noreply.github.com>
2024-05-23 12:20:41 +0900

    @OneToOne 단방향 테스트

---
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -0,0 +1,21 @@
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "food")
+public class Food { // 얘가 외래키의 주인!!!
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
